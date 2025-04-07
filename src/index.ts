373be240da1d32b065e6756b8e7b428b2f262a85
dhuruv bansal <dhuruvbansal99@gmail.com>
2025-04-07 10:39:54 +0530

    done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log("hello");
+import { createClient } from "redis";
+
+let time = 0;
+let int: NodeJS.Timeout | null;
+
+const StartInterval = () => {
+  int = setInterval(() => {
+    time = time + 1;
+  }, 1);
+};
+StartInterval();
+
+const stopInterval = () => {
+  if (int) clearInterval(int);
+};
+
+const client = createClient();
+async function setTest() {
+  await client.connect();
+  const res = await client.set("test", "working");
+  console.log(res);
+  await client.quit();
+}
+async function getTest() {
+  await client.connect();
+  const res = await client.get("test");
+  console.log(res);
+  await client.quit();
+  stopInterval();
+  console.log(time);
+}
+getTest();
